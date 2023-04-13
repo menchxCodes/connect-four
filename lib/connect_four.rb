f58a27119262f158cb2ccfe204984cf1564975a0
menchxCodes <43462510+menchxCodes@users.noreply.github.com>
2023-04-13 19:59:38 +0330

    Create Player class & add to Board class initialization tests

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,6 +1,16 @@
 class Board
-  attr_reader :board
-  def initialize(col_count = 7, row_count = 6)
+  attr_reader :board, :player_one, :player_two
+  def initialize(col_count = 7, row_count = 6, player_one = 'You', player_two = 'Computer')
     @board = Array.new(col_count) { Array.new(row_count, ' ') }
+    @player_one = Player.new( player_one, "\u26AA")
+    @player_two = Player.new(player_two, "\u26AB")
+    @current_player = @player_one
+  end
+end
+
+class Player
+  def initialize(name, sign)
+    @name = name
+    @sign = sign
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,8 +1,9 @@
 require './lib/connect_four'
 describe Board do
   describe '#initialize' do
+    subject(:game_board) { described_class.new }
+
     context 'board is created correctly' do
-      subject(:game_board) { described_class.new }
       let(:board) { game_board.board }
       it 'board has the correct # of columns' do
         expect(board.size).to eq(7)
@@ -18,6 +19,24 @@ describe Board do
       it 'all cells are clear' do
         all_cells = board.flatten
         expect(all_cells).to all(be_clear)
+      end
+    end
+
+    context 'players are initialized correctly' do
+      it 'player one is created ' do
+        player_one = game_board.instance_variable_get(:@player_one)
+        expect(player_one).to be_instance_of(Player)
+      end
+
+      it 'player two is created' do
+        player_two = game_board.instance_variable_get(:@player_two)
+        expect(player_two).to be_instance_of(Player)
+      end
+
+      it 'current player is player_one' do
+        player_one = game_board.instance_variable_get(:@player_one)
+        current_player = game_board.instance_variable_get(:@current_player)
+        expect(current_player).to be(player_one)
       end
     end
   end
